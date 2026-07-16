f127d35aa99a6773efdbec738eeeeda989b8e831
Michael Miller <mkmiller06@icloud.com>
2026-07-16 13:58:45 +0800

    first commit
    first commit

---
+++ Apple Foundations Recipe Box/MyRecipe.swift
@@ -0,0 +1,18 @@
+//
+//  MyRecipe.swift
+//  Apple Foundations Recipe Box
+//
+//  Created by Michael Miller on 16/7/2026.
+//
+
+import SwiftUI
+
+struct MyRecipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyRecipe()
+}
