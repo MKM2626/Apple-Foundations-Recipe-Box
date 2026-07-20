a7d5c7e7225d0e1b0899dce59c3bfad7ea5be6ed
Michael Miller <mkmiller06@icloud.com>
2026-07-20 11:22:43 +0800

    added team files, and changed file names

--- Apple Foundations Recipe Box/Recipe data/GabrielRecipeModel.swift
+++ Apple Foundations Recipe Box/Recipe data/GabrielRecipeModel.swift
@@ -1,18 +1,38 @@
-//
-//  GabrielRecipeModel.swift
-//  Apple Foundations Recipe Box
-//
-//  Created by Michael Miller on 20/7/2026.
-//
-
 import SwiftUI
-
-struct GabrielRecipeModel: View {
+ 
+struct RecipeCard: View {
+    var recipe: Recipe
+    @State private var isSaved = false
+ 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 12) {
+            Image("prawn")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 100)
+                .frame(maxWidth: 140)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+ 
+            HStack {
+                Text(recipe.name)
+                    .font(.title2)
+                    .bold()
+ 
+                Button {
+                    isSaved.toggle()
+                } label: {
+                    Image(systemName: isSaved ? "heart.fill" : "heart")
+                        .foregroundColor(isSaved ? .red : .gray)
+                }
+            }
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(12)
+        .shadow(radius: 4)
     }
 }
-
+ 
 #Preview {
-    GabrielRecipeModel()
+    RecipeCard(recipe: myRecipeData.recipes[0])
 }
--- Apple Foundations Recipe Box/Recipe data/RecipeModel.swift
+++ Apple Foundations Recipe Box/Recipe data/RecipeModel.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import SwiftUI
 
 struct Recipe: Identifiable {
     var id = UUID()
