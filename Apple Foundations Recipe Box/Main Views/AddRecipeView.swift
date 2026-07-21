41064faf2559bf9f86bfd7bd81f50c680d9b18f4
Michael Miller <mkmiller06@icloud.com>
2026-07-21 13:06:15 +0800

    add recipe view

---
+++ Apple Foundations Recipe Box/Main Views/AddRecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  AddRecipeView.swift
+//  Apple Foundations Recipe Box
+//
+//  Created by Michael Miller on 21/7/2026.
+//
+
+import SwiftUI
+
+struct AddRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddRecipeView()
+}
